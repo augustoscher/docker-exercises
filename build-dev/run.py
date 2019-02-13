41d214069d320f631f60214910d2137294408d9f
augusto.scher <augusto.scher@senior.com.br>
2019-02-13 15:11:27 -0200

    alterando servidor python

--- build-dev/run.py
+++ build-dev/run.py
@@ -1,26 +1,26 @@
 import logging
 import http.server
-import SocketServer
+import socketserver
 import getpass
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
-   def log_message(self, format, *args):
-       logging.info("%s - - [%s] %s\n"% (
-           self.client_address[0],
-           self.log_date_time_string(),
-           format%args
-       ))
+    def log_message(self, format, *args):
+        logging.info("%s - - [%s] %s\n"% (
+            self.client_address[0],
+            self.log_date_time_string(),
+            format%args
+        ))
 
 logging.basicConfig(
-   filename='/log/http-server.log',
-   format='%(asctime)s - %s(levelname)s - %(message)s',
-   level=logging.INFO
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 logging.getLogger().addHandler(logging.StreamHandler())
-logging.info('inicializando ...')
+logging.info('inicializando...')
 PORT = 8000
 
-httpd = SocketServer.TCPServer(("", PORT), MyHTTPHandler)
+httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
 logging.info('escutando a porta: %s', PORT)
-logging.info('usuario: %s', getpass.getuser())
+logging.info('usuário: %s', getpass.getuser())
 httpd.serve_forever()
